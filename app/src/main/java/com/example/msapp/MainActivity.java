9148b0e7cbcba22802c81c154c09bb08076d545f
chetan salunke <chetansalunke79@gmail.com>
2023-11-15 01:54:36 +0530

    bottom UI completed

--- app/src/main/java/com/example/msapp/MainActivity.java
+++ app/src/main/java/com/example/msapp/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.msapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private NavController navController;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Find the NavHostFragment using its ID
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
 
+        // Get the NavController associated with the NavHostFragment
+        navController = navHostFragment.getNavController();
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-
-
+        // Associate the BottomNavigationView with the NavController
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
