bd943a5439b68e9c1a3298a982a66c636e04432d
chetan salunke <chetansalunke79@gmail.com>
2023-11-14 23:24:30 +0530

    Dashboard

---
+++ app/src/main/java/com/example/msapp/Fragment/profileFragment.java
@@ -0,0 +1,33 @@
+package com.example.msapp.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.msapp.R;
+
+
+public class profileFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
