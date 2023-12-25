fc6fd88be4a1511985986f8f62f2a1a5d82a2c34
chetan salunke <chetansalunke79@gmail.com>
2023-12-25 09:39:46 +0530

    back end changes

--- app/src/main/java/com/example/msapp/BookingActivity.java
+++ app/src/main/java/com/example/msapp/BookingActivity.java
@@ -1,6 +1,7 @@
 package com.example.msapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,9 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.msapp.databinding.ActivityBookingBinding;
 import com.example.msapp.databinding.ActivityOtpSendBinding;
@@ -22,6 +26,11 @@ public class BookingActivity extends AppCompatActivity {
     String[] items={"Baner","Kothrud","Shivaji Nagar","Bhukum","Hinjwadi"};
 
     TextInputLayout addressEt;
+    TextView selectTimeTv;
+    Toolbar appBar;
+    TextView selectAgeTv;
+    TextView addOnTv;
+    TextView workerPrefTv;
     RadioGroup workergenderRg;
     Spinner locationSpinner;
     CheckBox dishWashingCb;
@@ -32,6 +41,8 @@ public class BookingActivity extends AppCompatActivity {
     CheckBox eveningCb;
     CheckBox nightCb;
     RadioGroup packageRg;
+    RadioGroup selectAgeRg;
+    RadioGroup workerPrefRg;
 
     Button bokingBtn;
 
@@ -52,24 +63,122 @@ public class BookingActivity extends AppCompatActivity {
 
         locationSpinner = binding.locationSpinner;
         addressEt = binding.addressEt;
-
+        selectAgeTv = binding.selectAgeTv;
+        selectAgeRg = binding.slectAgeRg;
+        eveningCb = binding.checkBox6;
+        addOnTv = binding.addOnTv;
+        dishWashingCb = binding.checkBox1;
+        broomingMoppingCb = binding.checkBox2;
+        dustingCb = binding.checkBox3;
+        workerPrefTv = binding.workerPrefTv;
+        workergenderRg = binding.workergenderRg;
+        selectTimeTv = binding.selectTimeTv;
+        morinigCb = binding.checkBox4;
+        afternoon = binding.checkBox5;
+        nightCb = binding.checkBox7;
 
         Intent intent = getIntent();
 
-        String domesticService = intent.getStringExtra("domesticService");
-        String cookigService = intent.getStringExtra("cookigService");
-        String babysitterSerivce = intent.getStringExtra("babysitterSerivce");
-        String patientcareService = intent.getStringExtra("patientcareService");
-        String eldercareService = intent.getStringExtra("eldercareService");
-        String petcareService = intent.getStringExtra("petcareService");
-        String officeboyService = intent.getStringExtra("officeboyService");
-        String driverService = intent.getStringExtra("driverService");
+        String clicked_identifer = intent.getStringExtra("clicked_identifer");
 
-        if( domesticService =="domesticService")
+
+        if( clicked_identifer.equalsIgnoreCase("domesticService"))
         {
-            locationSpinner.setVisibility(View.GONE);
-            addressEt.setVisibility(View.GONE);
+            //appBar.setTitle("Domestic Service Booking");
+            selectAgeTv.setVisibility(View.GONE);
+            selectAgeRg.setVisibility(View.GONE);
+        }else if(clicked_identifer.equalsIgnoreCase("cookigService")){
+            selectAgeTv.setVisibility(View.GONE);
+            selectAgeRg.setVisibility(View.GONE);
+            eveningCb.setVisibility(View.GONE);
+            addOnTv.setVisibility(View.GONE);
+            dishWashingCb.setVisibility(View.GONE);
+            broomingMoppingCb.setVisibility(View.GONE);
+            dustingCb.setVisibility(View.GONE);
+
+        }else if(clicked_identifer.equalsIgnoreCase("babysitterSerivce"))
+        {
+            workerPrefTv.setVisibility(View.GONE);
+            workergenderRg.setVisibility(View.GONE);
+            addOnTv.setVisibility(View.GONE);
+            dishWashingCb.setVisibility(View.GONE);
+            broomingMoppingCb.setVisibility(View.GONE);
+            dustingCb.setVisibility(View.GONE);
+            eveningCb.setVisibility(View.GONE);
+        }else if(clicked_identifer.equalsIgnoreCase("eldercareService")){
+            workerPrefTv.setVisibility(View.GONE);
+            workergenderRg.setVisibility(View.GONE);
+            addOnTv.setVisibility(View.GONE);
+            dishWashingCb.setVisibility(View.GONE);
+            broomingMoppingCb.setVisibility(View.GONE);
+            dustingCb.setVisibility(View.GONE);
+            eveningCb.setVisibility(View.GONE);
+            selectTimeTv.setVisibility(View.GONE);
+            morinigCb.setVisibility(View.GONE);
+            afternoon.setVisibility(View.GONE);
+            nightCb.setVisibility(View.GONE);
+            selectAgeTv.setVisibility(View.GONE);
+            selectAgeRg.setVisibility(View.GONE);
+        }else if(clicked_identifer.equalsIgnoreCase("patientcareService")){
+            workerPrefTv.setVisibility(View.GONE);
+            workergenderRg.setVisibility(View.GONE);
+            addOnTv.setVisibility(View.GONE);
+            dishWashingCb.setVisibility(View.GONE);
+            broomingMoppingCb.setVisibility(View.GONE);
+            dustingCb.setVisibility(View.GONE);
+            eveningCb.setVisibility(View.GONE);
+            selectTimeTv.setVisibility(View.GONE);
+            morinigCb.setVisibility(View.GONE);
+            afternoon.setVisibility(View.GONE);
+            nightCb.setVisibility(View.GONE);
+            selectAgeTv.setVisibility(View.GONE);
+            selectAgeRg.setVisibility(View.GONE);
         }
+        else if(clicked_identifer.equalsIgnoreCase("petcareService")){
+            workerPrefTv.setVisibility(View.GONE);
+            workergenderRg.setVisibility(View.GONE);
+            addOnTv.setVisibility(View.GONE);
+            dishWashingCb.setVisibility(View.GONE);
+            broomingMoppingCb.setVisibility(View.GONE);
+            dustingCb.setVisibility(View.GONE);
+            eveningCb.setVisibility(View.GONE);
+            selectTimeTv.setVisibility(View.GONE);
+            morinigCb.setVisibility(View.GONE);
+            afternoon.setVisibility(View.GONE);
+            nightCb.setVisibility(View.GONE);
+            selectAgeTv.setVisibility(View.GONE);
+            selectAgeRg.setVisibility(View.GONE);
+        }
+        else if(clicked_identifer.equalsIgnoreCase("officeboyService")){
+            workerPrefTv.setVisibility(View.GONE);
+            workergenderRg.setVisibility(View.GONE);
+            addOnTv.setVisibility(View.GONE);
+            dishWashingCb.setVisibility(View.GONE);
+            broomingMoppingCb.setVisibility(View.GONE);
+            dustingCb.setVisibility(View.GONE);
+            eveningCb.setVisibility(View.GONE);
+            selectTimeTv.setVisibility(View.GONE);
+            morinigCb.setVisibility(View.GONE);
+            afternoon.setVisibility(View.GONE);
+            nightCb.setVisibility(View.GONE);
+            selectAgeTv.setVisibility(View.GONE);
+            selectAgeRg.setVisibility(View.GONE);}
+        else if(clicked_identifer.equalsIgnoreCase("driverService")){
+                workerPrefTv.setVisibility(View.GONE);
+                workergenderRg.setVisibility(View.GONE);
+                addOnTv.setVisibility(View.GONE);
+                dishWashingCb.setVisibility(View.GONE);
+                broomingMoppingCb.setVisibility(View.GONE);
+                dustingCb.setVisibility(View.GONE);
+                eveningCb.setVisibility(View.GONE);
+                selectTimeTv.setVisibility(View.GONE);
+                morinigCb.setVisibility(View.GONE);
+                afternoon.setVisibility(View.GONE);
+                nightCb.setVisibility(View.GONE);
+                selectAgeTv.setVisibility(View.GONE);
+                selectAgeRg.setVisibility(View.GONE);
+            }
+
 
 
 
