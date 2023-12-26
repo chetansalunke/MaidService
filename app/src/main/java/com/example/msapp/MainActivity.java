6e910027d67c7a00f92a9223cf57e096b4ae0923
chetan salunke <chetansalunke79@gmail.com>
2023-12-26 20:20:28 +0530

    back end changes

--- app/src/main/java/com/example/msapp/MainActivity.java
+++ app/src/main/java/com/example/msapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.msapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -9,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +20,34 @@ public class MainActivity extends AppCompatActivity {
     private NavController navController;
     private BottomNavigationView bottomNavigationView;
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    Toolbar toolbar ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigationview);
+        toolbar = findViewById(R.id.appBar);
+
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navopen,R.string.navclose);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+
+
+
+
+
 
         // Find the NavHostFragment using its ID
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
@@ -31,9 +59,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Associate the BottomNavigationView with the NavController
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
-
-
 
     }
 }
